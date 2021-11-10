85a2a394a2221c6664cd4565242ba6aaeb079daf
nikita <shamaev090299@gmail.com>
2021-11-11 01:27:29 +0300

    right navigation + background

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-        <div>Hello, world!</div>
-        <div>Hello 2</div>
+    <div className="header-container">
+        <div className="div-left">
+            <div className="">asd</div>
+        </div>
+        <div className="div-right">
+            <div className="right-navigation">
+                <div>
+                    <a href="http://www.stackoverflow.org">
+                        <img alt="Поддержка" src="https://cdn0.iconfinder.com/data/icons/entypo/92/help-512.png"/>
+                    </a>
+                </div>
+                <div >
+                    <a href="http://www.stackoverflow.org">
+                        <img alt="Настройки" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/WMF-Agora-Settings_808080.svg/768px-WMF-Agora-Settings_808080.svg.png"/>
+                    </a>
+                </div>
+                <div>
+                    <a href="http://www.stackoverflow.org">
+                        <img alt="Приложения Google" src="http://cdn.onlinewebfonts.com/svg/img_85968.png"/>
+                    </a>
+                </div>
+                <div>
+                    <a href="http://www.stackflow.org">
+                        <img alt="профиль" src="http://cdn.onlinewebfonts.com/svg/img_568656.png"/>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
